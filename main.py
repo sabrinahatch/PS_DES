da69fe91a8c8e24f60829e155cbfdc617f4dd089
Sabrina Hatch <68970386+sabrinahatch@users.noreply.github.com>
2023-07-13 10:34:19 -0400

    set up some ideas for debugging

--- main.py
+++ main.py
@@ -2,6 +2,13 @@
 # July 10 2023
 # CMU REUSE
 # Program to simulate PS scheduling policy on a network with a exponential job size distribution
+
+# generate my own arrival times and examples and run through print statements in the program
+# generate more departures??
+# inc the length of experiment
+# run comparison for 70% load and compare for SPRT
+# randomly generated and created by hand
+# run 10 departures and read code carefully + go over pseudocode
 import numpy as np
 
 class Job:
@@ -9,7 +16,7 @@ class Job:
         self.arrivalTime = arrivalTime
         self.size = size
         self.rpt = rpt
-        self.departureTime = None
+        self.pDepartureTime = None
         self.completionTime = None
 
 # Function to generate job sizes
@@ -18,7 +25,7 @@ def generateJobSize():
 
 # Function that generates an interarrival time
 def generateInterarrivalTime():
-    return np.random.exponential(10 / 8)
+    return np.random.exponential(10 / 1)
 
 def handleArr():
     global clock, lastEvent, departures, nextArrTime, nextDepTime, completionTimes, jobSizes, departingJob, rate, servingList
@@ -32,12 +39,12 @@ def handleArr():
 
     print("this is the servingList: " + str(servingList))
     for x in servingList:
-        print("This is the jobsize " + str(x.size) + " of " + str(x))
-        print("This is the rate " + str(rate))
-        print("This is the rpt before: " + str(x.rpt) + " of " + str(x))
-        print("This is the time elapsed " + str(clock - lastEvent))
+        # print("This is the jobsize " + str(x.size) + " of " + str(x))
+        # print("This is the rate " + str(rate))
+        # print("This is the rpt before: " + str(x.rpt) + " of " + str(x))
+        # print("This is the time elapsed " + str(clock - lastEvent))
         x.rpt = x.rpt - (clock - lastEvent) * (1 / len(servingList))
-        print("this is the rpt of job after " + str(x) + " " + str(x.rpt))
+        # print("this is the rpt of job after " + str(x) + " " + str(x.rpt))
 
         print("\n")
 
@@ -52,16 +59,16 @@ def handleArr():
     # update the new rate for the server
     rate = (1/len(servingList))
 
-    # update all the predicted departureTimes based on the newly calculated rate
+    # update all the predicted pDepartureTimes based on the newly calculated rate
     for x in servingList:
-        x.departureTime = ((x.rpt)/rate) + clock
+        x.pDepartureTime = ((x.rpt)/rate) + clock
 
     # generate the nextArrTime
 
 
     # find the job with the min departure time and set that departure time equal to the next dep time
-    departingJob = min(servingList, key = lambda x:x.departureTime)
-    nextDepTime = departingJob.departureTime
+    departingJob = min(servingList, key = lambda x:x.pDepartureTime)
+    nextDepTime = departingJob.pDepartureTime
     job = None
     nextArrTime = clock + generateInterarrivalTime()
 
@@ -70,12 +77,13 @@ def handleDep():
     global clock, lastEvent, departures, nextArrTime, nextDepTime, completionTimes, jobSizes, departingJob, rate, servingList
     departures += 1
 
-    departingJob.completionTime = departingJob.departureTime - departingJob.arrivalTime
+    departingJob.completionTime = departingJob.pDepartureTime - departingJob.arrivalTime
+    print("New departure of " + str(departingJob))
     if departures == maxDepartures:
         completionTimes.append(departingJob.completionTime)
-        print("New departure of " + str(departingJob))
     servingList.remove(departingJob) # equivalent to pop first from the list
 
+    print("this is the rpt of the departing job " + str(departingJob.rpt))
 
 
     if len(servingList) != 0:
@@ -88,19 +96,20 @@ def handleDep():
 
 
         for x in servingList:
-            x.departureTime = (x.rpt / rate) + clock
-        departingJob = min(servingList, key=lambda x: x.departureTime)  # this will be the head of the queue
-        nextDepTime = departingJob.departureTime
+            x.pDepartureTime = (x.rpt / rate) + clock
+        departingJob = min(servingList, key=lambda x: x.pDepartureTime)  # this will be the head of the queue
+        nextDepTime = departingJob.pDepartureTime
+        #print("this is the rpt of the departing job " + str(departingJob.rpt))
     else:
         departingJob = None
         nextDepTime = float("inf")
         lastEvent = clock
 # The following lines of code run the logic of the simulation
 seed = 0
-maxDepartures = 5
+maxDepartures = 20000
 completionTimes = []
 count = 0
-runs = 2
+runs = 5000
 for i in range(runs):
     count += 1
     np.random.seed(seed)
@@ -126,12 +135,12 @@ for i in range(runs):
     seed += 1
 
 
-# load data into a txt file
-# with open("PS_LOAD_0.8.txt", "w") as fp:
-#     for item in completionTimes:
-#         fp.write("%s\n" % item)
-#
-#
+#load data into a txt file
+with open("PS_LOAD_0.1.txt", "w") as fp:
+    for item in completionTimes:
+        fp.write("%s\n" % item)
+
+
 
 
 
