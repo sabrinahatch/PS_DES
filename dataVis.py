e4c6dd28b63f0e553b9eca6aba4afe27b6d02feb
Sabrina <sabrinahatch222@gmail.com>
2023-07-26 11:36:38 -0400

    final product.

--- dataVis.py
+++ dataVis.py
@@ -1,7 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-loads = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7]
+loads = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]
 
 x = np.zeros(len(loads))
 y = np.zeros(len(loads))
